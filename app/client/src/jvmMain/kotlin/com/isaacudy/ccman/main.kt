9e5fd42b36656539c50e0e9587c78d83606963fc
Isaac Udy (AI) <isaac.udy.ai@gmail.com>
2026-03-30 03:42:20 +1300

    Add back/close naivigation to the desktop app

--- app/client/src/jvmMain/kotlin/com/isaacudy/ccman/main.kt
+++ app/client/src/jvmMain/kotlin/com/isaacudy/ccman/main.kt
@@ -1,10 +1,18 @@
 package com.isaacudy.ccman
 
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.application
+import dev.enro.NavigationKey
+import dev.enro.context.activeLeaf
+import dev.enro.context.getNavigationHandle
 import dev.enro.platform.desktop.GenericRootWindow
 import dev.enro.platform.desktop.RootWindow
 import dev.enro.platform.desktop.openWindow
+import dev.enro.requestClose
 import dev.enro.ui.EnroApplicationContent
+import dev.enro.ui.LocalNavigationContext
 
 fun main() {
     val controller = ApplicationNavigation.installNavigationController(Unit)
@@ -16,6 +24,29 @@ fun main() {
                 )
             }
         ) {
+            val context = LocalNavigationContext.current
+            MenuBar {
+                Menu("Window") {
+                    Item(
+                        "Back",
+                        shortcut = KeyShortcut(
+                            key = Key.LeftBracket,
+                            meta = true
+                        )
+                    ) {
+                        context.activeLeaf().getNavigationHandle<NavigationKey>().requestClose()
+                    }
+                    Item(
+                        "Close",
+                        shortcut = KeyShortcut(
+                            key = Key.W,
+                            meta = true
+                        )
+                    ) {
+                        context.activeLeaf().getNavigationHandle<NavigationKey>().requestClose()
+                    }
+                }
+            }
             ApplicationContent()
         }
     )
